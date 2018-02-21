5b170f6907b0eb618e00dca98fbc153c69cd0e1a
sercan.kulig <sercankulig@gmail.com>
2018-02-21 12:26:46 +0300

    Aspect Around State

--- src/main/java/com/article/aspect/LoggerAspect.java
+++ src/main/java/com/article/aspect/LoggerAspect.java
@@ -2,9 +2,12 @@ package com.article.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 
 @Aspect
@@ -19,9 +22,9 @@ public class LoggerAspect {
     }
 
     @AfterThrowing(pointcut = "generalPointcut() throw Exception", throwing = "ex")
-    public void exceptionLog(JoinPoint joinPoint, Exception ex) throws Exception {
+    public void exceptionLog(JoinPoint joinPoint, Throwable ex) {
         logger.info(joinPoint.getTarget().getClass().getSimpleName() + " : " + joinPoint.getSignature().getName()
-        + " : " + ex.getMessage());
+                + " : " + ex.getMessage());
     }
 
     @Before("generalPointcut()")
@@ -29,9 +32,24 @@ public class LoggerAspect {
         logger.info(joinPoint.getTarget().getClass().getSimpleName() + " : " + joinPoint.getSignature().getName());
     }
 
-/*    @Around("generalPointcut()")
-    public void aroundInfoLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger.info(Arrays.toString(joinPoint.getArgs()));
-        joinPoint.proceed(); //continue on the intercepted method
-    }*/
+    @Around("generalPointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Enter: {}.{}() with argument[s] = {}" + joinPoint.getSignature().getDeclaringTypeName() + " : " +
+                    joinPoint.getSignature().getName() + " : " + Arrays.toString(joinPoint.getArgs()));
+        }
+        try {
+            Object result = joinPoint.proceed();
+            if (logger.isDebugEnabled()) {
+                logger.debug("Exit: {}.{}() with result = {}" + " : " + joinPoint.getSignature().getDeclaringTypeName() + " : " +
+                        joinPoint.getSignature().getName() + " : " + result);
+            }
+            return result;
+        } catch (IllegalArgumentException e) {
+            logger.error("Illegal argument: {} in {}.{}()" + " : " + Arrays.toString(joinPoint.getArgs()) + " : " +
+                    joinPoint.getSignature().getDeclaringTypeName() + " : " + joinPoint.getSignature().getName());
+
+            throw e;
+        }
+    }
 }
